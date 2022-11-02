5e533cce2319471ef776cd12f478af4221fff665
Jacob Gilbert <jacobgilb03@gmail.com>
2022-11-02 17:06:16 -0400

    Deploy

--- core/models.py
+++ core/models.py
@@ -4,3 +4,15 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Habit(models.Model):
+    name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Record(models.Model):
+    pass
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import User, Record
 
 # Create your views here.
